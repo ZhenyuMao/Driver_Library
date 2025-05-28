0c6ac49dec8a5ea9a8477d9d07085b37473a10d2
MaoZhenYu <geekzhenyu@outlook.com>
2025-05-28 17:53:50 +0800

    init project

--- Clion/cmake-build-debug/cmake_install.cmake
+++ Clion/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/project/Driver_Library/Clion
+# Install script for directory: C:/project/Driver_Labrary/Clion
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/project/Driver_Library/Clion/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/project/Driver_Labrary/Clion/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
